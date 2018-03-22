5890e2d7f563619ea7f78e9b16ce2064afcef0ad
Alex Bamford <Bamford@Bamfords-MacBook-Pro.local>
2018-03-22 12:14:54 +0000

    script

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
-var iframe = document.getElementById('frameID'),
-    iframeWin = iframe.contentWindow || iframe,
-    iframeDoc = iframe.contentDocument || iframeWin.document;
-
-$(iframeDoc).ready(function (event) {
-    iframeDoc.open();
-    iframeDoc.write('<script src="//c.fqtag.com/tag/implement-r.js?org=TksRHGen5rmjsG48m9pC&s=a&p=TEST&a=TEST&cmp=TEST&fmt=banner&app=com.TEST.TEST&rt=display&sl=1&fq=1"></script>');
-    iframeDoc.close();
-});
+var iframe = document.getElementById('frameID');
+iframe = iframe.contentWindow || iframe.contentDocument.document || iframe.contentDocument;
+iframe.document.open();
+iframe.write('<script src="//c.fqtag.com/tag/implement-r.js?org=TksRHGen5rmjsG48m9pC&s=a&p=TEST&a=TEST&cmp=TEST&fmt=banner&app=com.TEST.TEST&rt=display&sl=1&fq=1"></script>');
+iframe.close();

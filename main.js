7196db507af73af0ff4f6d300aa4c7abe21d4f96
Alex Bamford <Bamford@Bamfords-MacBook-Pro.local>
2018-03-22 15:48:43 +0000

    script

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ var pageUrl = window.location.href
 var iframe = document.getElementById('frameID');
 iframe = iframe.contentWindow || iframe.contentDocument.document || iframe.contentDocument;
 iframe.document.open();
-iframe.document.write('<script src="//c.fqtag.com/tag/implement-r.js?org=TksRHGen5rmjsG48m9pC&p=bamfordsTestPage&a=testpage&cmp=github&fmt=banner&rd='+pageUrl+'app=com.itis.whatitis&rt=display&sl=1&fq=1"></script>');
+iframe.document.write('<script src="//c.fqtag.com/tag/implement-r.js?org=TksRHGen5rmjsG48m9pC&p=bamfordsTestPage&a=testpage&cmp=github&fmt=banner&rd='+pageUrl+'&rt=display&sl=1&fq=1"></script>');
 iframe.document.close();

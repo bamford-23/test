fc81b6e64bacf3bbb21f7eb522d8711ea7db08d6
Alex Bamford <Bamford@Bamfords-MacBook-Pro.local>
2018-03-22 16:43:54 +0000

    script

--- main.js
+++ main.js
@@ -4,4 +4,5 @@ var iframe = document.getElementById('frameID');
 iframe = iframe.contentWindow || iframe.contentDocument.document || iframe.contentDocument;
 iframe.document.open();
 iframe.document.write('<script src="//c.fqtag.com/tag/implement-r.js?org=TksRHGen5rmjsG48m9pC&p=bamfordsTestPage&a=testpage&cmp=github&fmt=banner&rd='+pageUrl+'&rt=display&sl=1&fq=1"></script>');
+iframe.document.write('<img src="https://i.imgur.com/oJW0Rs4.png" width="636" height="501>');
 iframe.document.close();

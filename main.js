b1d8fbed51dc826a8d907da2b6e0c4dd9751743a
Alex Bamford <Bamford@Bamfords-MacBook-Pro.local>
2018-04-27 10:44:20 +0100

    small changes

--- main.js
+++ main.js
@@ -5,3 +5,5 @@ iframe = iframe.contentWindow || iframe.contentDocument.document || iframe.conte
 iframe.document.open();
 iframe.document.write('<script src="//c.fqtag.com/tag/implement-r.js?org=TksRHGen5rmjsG48m9pC&p=bamfordsTestPage&a=testpage&cmp=github&fmt=banner&dmn=http://any.dominayoulike.com/thiswont/doanything&rt=display&sl=1&fq=1"></script>');
 iframe.document.close();
+
+// &rd="+pageUrl+"  - Add this back in after the crapola.

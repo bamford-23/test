25bb5598321ecf50d8bb85a694b92d32da717bd6
Alex Bamford <Bamford@Bamfords-MacBook-Pro.local>
2018-04-27 10:05:46 +0100

    changes

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var pageUrl = window.location.href
+// var pageUrl = window.location.href
 
 var iframe = document.getElementById('frameID');
 iframe = iframe.contentWindow || iframe.contentDocument.document || iframe.contentDocument;
 iframe.document.open();
-iframe.document.write('<script src="//c.fqtag.com/tag/implement-r.js?org=TksRHGen5rmjsG48m9pC&p=bamfordsTestPage&a=testpage&cmp=github&fmt=banner&rd='+pageUrl+'&rt=display&sl=1&fq=1"></script>');
+iframe.document.write('<script src="//c.fqtag.com/tag/implement-r.js?org=TksRHGen5rmjsG48m9pC&p=bamfordsTestPage&a=testpage&cmp=github&fmt=banner&dmn=http://any.dominayoulike.com/thiswont/doanything&rt=display&sl=1&fq=1"></script>');
 iframe.document.close();

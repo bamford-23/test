72abc848df55882c7bf6b19fab909401e535fc15
Alex Bamford <Bamford@Bamfords-MacBook-Pro.local>
2018-06-28 14:06:14 +0100

    tring with ad element

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 // var pageUrl = window.location.href
 
-var iframe = document.getElementById('adframe');
+var iframe = document.getElementById('adframe1');
 iframe = iframe.contentWindow || iframe.contentDocument.document || iframe.contentDocument;
 iframe.document.open();
-iframe.document.write('<script src="//c.fqtag.com/tag/implement-r.js?org=TksRHGen5rmjsG48m9pC&p=bamfordsTestPage&a=testpage&cmp=github&fmt=banner&dmn=http://any.dominayoulike.com/thiswont/doanything&rt=display&sl=1&fq=1"></script>');
+iframe.document.write('<script src="//c.fqtag.com/tag/implement-r.js?org=TksRHGen5rmjsG48m9pC&p=bamfordsTestPage&a=testpage&cmp=adframe1&fmt=banner&dmn=http://any.dominayoulike.com/thiswont/doanything&rt=display&sl=1&fq=1"></script>');
 iframe.document.close();
 
-var iframe = document.evaluate('//iframe[@data-aa="946803"]', document, null, XPathResult.ANY_TYPE, null).iterateNext();
-var tagScript = document.createElement('script');
-tagScript.src = "//c.fqtag.com/tag/implement-r.js?org=TksRHGen5rmjsG48m9pC&p=bamfordsTestPage&a=testpage&cmp=github&fmt=banner&dmn=http://any.dominayoulike.com/thiswont/doanything&rt=display&sl=1&fq=1";
-iframe.contentDocument.body.appendChild(tagScript)
+// This doesn't work - you can't do this to an iFrame
+// var iframe = document.evaluate('//iframe[@data-aa="946803"]', document, null, XPathResult.ANY_TYPE, null).iterateNext();
+// var tagScript = document.createElement('script');
+// tagScript.src = "//c.fqtag.com/tag/implement-r.js?org=TksRHGen5rmjsG48m9pC&p=bamfordsTestPage&a=testpage&cmp=github&fmt=banner&dmn=http://any.dominayoulike.com/thiswont/doanything&rt=display&sl=1&fq=1";
+// iframe.contentDocument.body.appendChild(tagScript)
 
 // &rd="+pageUrl+"  - Add this back in after the crapola.

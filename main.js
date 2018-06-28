6fb6446248930a8ac0743f88d876883415e254b9
Alex Bamford <Bamford@Bamfords-MacBook-Pro.local>
2018-06-28 12:33:15 +0100

    changed frame

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // var pageUrl = window.location.href
 
-var iframe = document.getElementById('frameID');
+var iframe = document.evaluate('//iframe[@data-aa="946803"]', document, null, XPathResult.ANY_TYPE, null).iterateNext();
 iframe = iframe.contentWindow || iframe.contentDocument.document || iframe.contentDocument;
 iframe.document.open();
 iframe.document.write('<script src="//c.fqtag.com/tag/implement-r.js?org=TksRHGen5rmjsG48m9pC&p=bamfordsTestPage&a=testpage&cmp=github&fmt=banner&dmn=http://any.dominayoulike.com/thiswont/doanything&rt=display&sl=1&fq=1"></script>');

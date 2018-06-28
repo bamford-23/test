dc91a055951e9b4d1d6ba26fae075586fb5dcb0a
Alex Bamford <Bamford@Bamfords-MacBook-Pro.local>
2018-06-28 13:32:07 +0100

    changing how the Script is written to the iframe

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 // var pageUrl = window.location.href
 
+// var iframe = document.evaluate('//iframe[@data-aa="946803"]', document, null, XPathResult.ANY_TYPE, null).iterateNext();
+// iframe = iframe.contentWindow || iframe.contentDocument.document || iframe.contentDocument;
+// iframe.document.open();
+// iframe.document.write('<script src="//c.fqtag.com/tag/implement-r.js?org=TksRHGen5rmjsG48m9pC&p=bamfordsTestPage&a=testpage&cmp=github&fmt=banner&dmn=http://any.dominayoulike.com/thiswont/doanything&rt=display&sl=1&fq=1"></script>');
+// iframe.document.close();
+
 var iframe = document.evaluate('//iframe[@data-aa="946803"]', document, null, XPathResult.ANY_TYPE, null).iterateNext();
-iframe = iframe.contentWindow || iframe.contentDocument.document || iframe.contentDocument;
-iframe.document.open();
-iframe.document.write('<script src="//c.fqtag.com/tag/implement-r.js?org=TksRHGen5rmjsG48m9pC&p=bamfordsTestPage&a=testpage&cmp=github&fmt=banner&dmn=http://any.dominayoulike.com/thiswont/doanything&rt=display&sl=1&fq=1"></script>');
-iframe.document.close();
+var tagScript = document.createElement('script');
+tagScript.src = "//c.fqtag.com/tag/implement-r.js?org=TksRHGen5rmjsG48m9pC&p=bamfordsTestPage&a=testpage&cmp=github&fmt=banner&dmn=http://any.dominayoulike.com/thiswont/doanything&rt=display&sl=1&fq=1";
+iframe.contentDocument.body.appendChild(tagScript)
 
 // &rd="+pageUrl+"  - Add this back in after the crapola.

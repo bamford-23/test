e77c160941fcc46e4613ca4d5ef4e4bf851d03d2
Alex Bamford <Bamford@Bamfords-MacBook-Pro.local>
2018-06-28 15:49:43 +0100

    adding div around ifame

--- main.js
+++ main.js
@@ -8,9 +8,11 @@ iframe.document.close();
 
 // This doesn't work - you can't do this to an iFrame
 // var iframe = document.evaluate('//iframe[@data-aa="946803"]', document, null, XPathResult.ANY_TYPE, null).iterateNext();
-// var tagScript = document.createElement('script');
-// tagScript.src = "//c.fqtag.com/tag/implement-r.js?org=TksRHGen5rmjsG48m9pC&p=bamfordsTestPage&a=testpage&cmp=github&fmt=banner&dmn=http://any.dominayoulike.com/thiswont/doanything&rt=display&sl=1&fq=1";
+
 // iframe.contentDocument.body.appendChild(tagScript)
 
 // &rd="+pageUrl+"  - Add this back in after the crapola.
 var footer = document.getElementById('footer');
+var tagScript = document.createElement('script');
+tagScript.src = "//c.fqtag.com/tag/implement-r.js?org=TksRHGen5rmjsG48m9pC&p=bamfordsTestPage&a=testpage&cmp=github&fmt=banner&dmn=http://any.dominayoulike.com/thiswont/doanything&rt=display&sl=1&fq=1";
+footer.appendChild(tagScript);

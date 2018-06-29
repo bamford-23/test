ca0afcf2558cee66fa12266aeab2aa3558e5979a
Alex Bamford <agfbamford@gmail.com>
2018-06-29 14:46:11 +0100

    neaten code

--- main.js
+++ main.js
@@ -6,10 +6,7 @@ iframe.document.open();
 iframe.document.write('<script src="//c.fqtag.com/tag/implement-r.js?org=TksRHGen5rmjsG48m9pC&p=bamfordsTestPage&a=testpage&cmp=drums&fmt=banner&dmn=http://any.dominayoulike.com/thiswont/doanything&rt=display&sl=1&fq=1"></script>');
 iframe.document.close();
 
-// This doesn't work - you can't do this to an iFrame
-// var iframe = document.evaluate('//iframe[@data-aa="946803"]', document, null, XPathResult.ANY_TYPE, null).iterateNe
-
-// &rd="+pageUrl+"  - Add this back in after the crapola.
+// This doesn't work - you can't add content to an iFrame without writing over the content: var iframe = document.evaluate('//iframe[@data-aa="946803"]', document, null, XPathResult.ANY_TYPE, null).iterateNe
 
 function addscript(){
   var footer = document.getElementById('footer');
@@ -18,3 +15,5 @@ function addscript(){
   footer.appendChild(tagScript);
 }
 setTimeout(addscript,200);
+
+// &rd="+pageUrl+"  - Add this back in the URL after testing.

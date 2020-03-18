9963dd9395157c3e81227a39aa5f687345786cae
Alex Bamford <agfbamford@gmail.com>
2020-03-18 12:29:19 +0000

    adding tracking link

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // var pageUrl = window.location.href
 
-var iframe = document.getElementById('drums');
-iframe = iframe.contentWindow || iframe.contentDocument.document || iframe.contentDocument;
-iframe.document.open();
-iframe.document.write('<script src="//c.fqtag.com/tag/implement-r.js?org=TksRHGen5rmjsG48m9pC&p=bamfordsTestPage&a=testpage&cmp=drums&fmt=banner&dmn=http://any.dominayoulike.com/thiswont/doanything&rt=display&sl=1&fq=1"></script>');
-iframe.document.close();
+// var iframe = document.getElementById('drums');
+// iframe = iframe.contentWindow || iframe.contentDocument.document || iframe.contentDocument;
+// iframe.document.open();
+// iframe.document.write('<script src="//c.fqtag.com/tag/implement-r.js?org=TksRHGen5rmjsG48m9pC&p=bamfordsTestPage&a=testpage&cmp=drums&fmt=banner&dmn=http://any.dominayoulike.com/thiswont/doanything&rt=display&sl=1&fq=1"></script>');
+// iframe.document.close();
 
 // This doesn't work - you can't add content to an iFrame without writing over the content: var iframe = document.evaluate('//iframe[@data-aa="946803"]', document, null, XPathResult.ANY_TYPE, null).iterateNe
 

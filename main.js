e256c5747b3aaa7de69834a2394c3f84827624cb
Bamford <alex.bamford@Alexs-MacBook-Air.local>
2022-05-05 15:03:40 +0100

    add retrieve cookie step

--- main.js
+++ main.js
@@ -97,6 +97,20 @@ function setCookie(cvalue, exdays) {
   let expires = "expires="+d.toUTCString();
   document.cookie = "irclickid=" + cvalue + ";" + expires + ";path=/";
 }
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
 
 
 
